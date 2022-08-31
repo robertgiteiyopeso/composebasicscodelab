baa9645d08c447082c65fecdeb0db02c22c36f44
Robert Gitei <robert.gitei@yopeso.com>
2022-08-31 13:05:37 +0300

    8. State hoisting

--- app/src/main/java/com/example/composebasicscodelab/MainActivity.kt
+++ app/src/main/java/com/example/composebasicscodelab/MainActivity.kt
@@ -4,13 +4,11 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.OutlinedButton
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -27,9 +25,18 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@Preview(showBackground = true, widthDp = 320)
 @Composable
-fun MyApp(names: List<String> = listOf("primul", "al doilea")) {
+fun MyApp() {
+    val shouldShowOnboarding = remember { mutableStateOf(true) }
+
+    if (shouldShowOnboarding.value)
+        OnboardingScreen (onContinueClicked = {shouldShowOnboarding.value = false})
+    else
+        Greetings()
+}
+
+@Composable
+fun Greetings(names: List<String> = listOf("primul", "al doilea")) {
     Column(modifier = Modifier.padding(vertical = 4.dp)) {
         for (name in names) {
             Greeting(name = name)
@@ -70,5 +77,32 @@ fun Greeting(name: String) {
 fun DefaultPreview() {
     ComposeBasicsCodelabTheme {
         Greeting("Android")
+    }
+}
+
+@Composable
+fun OnboardingScreen(onContinueClicked: () -> Unit) {
+    Surface {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text("Welcome to the Compose Basics Codelab!")
+            Button(
+                modifier = Modifier.padding(vertical = 24.dp),
+                onClick = { onContinueClicked() }
+            ) {
+                Text("Continue")
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true, widthDp = 320, heightDp = 320)
+@Composable
+fun OnboardingPreview() {
+    ComposeBasicsCodelabTheme {
+        OnboardingScreen(onContinueClicked = {})
     }
 }
